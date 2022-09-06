75528a2fe696001e6025b85311e477294d1492bd
Andreina Oliveira <andreinaholiveira@gmail.com>
2022-09-06 15:30:18 -0400

    refacttor: inserindo linhas

--- controller/log.py
+++ controller/log.py
@@ -6,8 +6,10 @@ logging.basicConfig(format=log_format, level=logging.INFO, filemode='w')
 def degub(message):
     logging.debug(message)
 
+
 def info(message):
     logging.info(message)
+
 
 def error(message):
     logging.error(message)
